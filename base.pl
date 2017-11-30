417aab143f9610c5855e918e056224be0ebb7920
Creusel <victor.creusel@gmail.com>
2017-11-30 19:10:30 +0100

    Correction de quelques bugs, et tests. Bugs si f(). Modifs pour les règles

--- base.pl
+++ base.pl
@@ -1,10 +1,21 @@
 /* Prédicats utiles */
 
+:- op(20,xfy,?=).
+
 fonc(X)
-:- functor(X, _, A), A > 0 .
+:- nonvar(X), functor(X, _, A), A > 0 .
 
 const(X)
 :- nonvar(X), functor(X, _, 0).
+
+
+
+/* Vérifie si X apparait dans T */
+
+occur_check(X, T)
+:- var(X), term_variables(T, Arguments), member(X, Arguments).
+
+
 
 
 /* Définition quelle regle appliquer en fonction de l'équation */
@@ -16,7 +27,7 @@ regle(simplify_r, X ?= T)
 :- var(X), const(T).
 
 regle(expand_r, X ?= T) 
-:- var(X), \+occur_check(X, T).
+:- var(X), fonc(T), \+occur_check(X, T).
 
 regle(check_r, X ?= T) 
 :- var(X), X \= T, occur_check(X, T).
@@ -30,26 +41,24 @@ regle(decompose_r, X ?= T)
 regle(clash_r, X ?= T) 
 :- fonc(X), fonc(T), functor(X, N1, A1), functor(T, N2, A2), A1 \= A2, N1 \= N2.
 
+regle(clash_r, X ?= T) 
+:- fonc(X), fonc(T), functor(X, N1, A1), functor(T, N2, A2), N1 \= N2.
+
+regle(clash_r, X ?= T) 
+:- fonc(X), fonc(T), functor(X, N1, A1), functor(T, N2, A2), A1 \= A2.
 
 
-/* Vérifie si X apparait dans T */
 
-occur_check(X, T)
-:- var(X), term_variables(T, Arguments), member(X, Arguments).
+/* règles de transformation */
+
+/* X: variable à renommer, Y: terme qui remplace X, L: liste où remplacer */
+rename_p(X, Y, [], Lout).
+
+rename_p(X, Y, Lin, [Z ?= T | Lout])
+:- rename_p(X, Y, [Z ?= T | Lin], Lout).
 
 
 /* Transforme P (couples) en Q (couples) en appliquant R (règle) sur E (équation tête de P) */
-
-reduit(rename_r, X ?= a, [X ?= a, Y ?= f(X)], L).
-
-
-/* reduit(R, E, P, [E | Q])
-<- reduit([ E | P], regle(E, R).*/
-
-reduit(R, E, [], Q). % Condition d'arrêt
-
-reduit(R, E, P, [E | Q])
-:- reduit(R, E, [E | P], Q), regle(R, E).
 
 
 
