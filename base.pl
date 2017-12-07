e86d11355d75fb548365e7897f94a64d35b9d7fc
Creusel <victor.creusel@gmail.com>
2017-12-07 19:03:57 +0100

    Writting rename, but won't work

--- base.pl
+++ base.pl
@@ -8,6 +8,9 @@ fonc(X)
 const(X)
 :- nonvar(X), functor(X, _, 0).
 
+pasunif(X)
+:- var(X) ; const(X) ; fonc(X).
+
 
 
 /* Vérifie si X apparait dans T */
@@ -59,35 +62,86 @@ rename_p(X, Y, Lin, [Z ?= T | Lout])
 /* Essais 03/12/2017 */
 
 /*check */
-reduit(check_r, E, _, bottom)
-:- regle(E, check_r).
+%fonctionne
+reduit(check_r, _, _, bottom).
+
 
 /*clash*/
-reduit(clash_r, E, _, bottom)
-:- regle(E, clash_r).
-
-
-%comment interroger
-%reduit(orient_r, t ?= X, P, Q).
+%fonctionne
+reduit(clash_r, _, _, bottom).
 
 
 /*orient*/
-reduit(orient_r, T ?= X, _, [X ?= T | _])
-:- regle(T ?= X, orient_r).
+%fonctionne
+reduit(orient_r, T ?= X, [T ?= X | P], [X ?= T | P]).
 
+
+/*decompose*/
+reduit(decompose_r, X ?= T, [X ?= T | P], Pout)
+:- X =.. [_ |F], T =.. [_ |G], decompose_aux(F, G, Q), append(P, Q, Pout).
 
 
 /*rename*/
-%reduit(rename_r, X ?= T, P, [Q | X ?= T])
-:- regle(T ?= X, rename_r) .
+/*reduit(rename_r, X ?= T, [X ?= T | P], Q)
+:- rename_aux(X, T, P, Q).*/
+
+/*rename_aux(_, _, [], _).*/ /*on s arrete quand c est vide*/
+
+/* cas où X = X1 et T = T1 */
+/*rename_aux(X, T, [X1 ?= T1| Pin], [T ?= T |Pout])
+:- same_term(X, X1), same_term(X, T1).*/
+
+/* cas où X = X1 */
+/*rename_aux(X, T, [X1 ?= T1| Pin], [T ?= T |Pout])
+:- same_term(X, X1), \+same_term(X, T1).*/
+
+/* cas où T = T1 */
+/*rename_aux(X, T, [X1 ?= T1| Pin], [X1 ?= T |Pout])
+:- \+same_term(X, X1), same_term(X, T1).*/
+
+/* cas où il n'y a rien à rename */
+/*rename_aux(X, T, [X1 ?= T1| Pin], [X1 ?= T1 |Pout])
+:- \+same_term(X, X1), \+same_term(X, T1).*/
 
 
+rename_terme(X, T, Terme, T)
+:- var(X), same_term(X, Terme).
+
+rename_terme(X, _, Terme, Terme)
+:- var(X), \+same_term(X, Terme), \+fonc(Terme).
+
+rename_terme(X, T, Terme, Sortie)
+:- var(X), \+same_term(X, Terme), fonc(Terme), Terme=..Liste, rename_list(X, T, Liste, ListeSortie), Sortie=..ListeSortie.
 
 
+rename_list(_, _, [], _).
+
+rename_list(X, T, [E | Lin], [Eout | Lout])
+:- rename_terme(X, T, E, Eout), rename_list(X, T, Lin, [Eout | Lout]).
 
 
+/*
+rename_terme(X, T, Terme, T)
+:- var(X), var(Terme), same_term(X, Terme), !.
+
+rename_terme(X, _, Terme, Terme)
+:- var(X), var(Terme), \+same_term(X, Terme), !.
+
+rename_terme(X, _, Terme, Terme)
+:- var(X), nonvar(Terme), \+same_term(X, Terme), \+fonc(Terme), !.
+
+rename_terme(X, T, Terme, Sortie)
+:- var(X), nonvar(Terme), \+same_term(X, Terme), fonc(Terme), 
+Terme=..Liste, rename_list(X, T, Liste, ListeSortie), Sortie=..ListeSortie, !.
+*/
 
 
+/*Lout reçoit des équations entre les éléments de L1 et L2*/
+decompose_aux([], [], _).
+
+
+decompose_aux([X1 | L1], [X2 | L2], Lout)
+:- decompose_aux(L1, L2, Lout), memberchk(X1 ?= X2, Lout).
 
 
 
@@ -97,17 +151,16 @@ reduit(orient_r, T ?= X, _, [X ?= T | _])
 /* unifie*/
 
 %unifie([X ?= t, X ?= f(X)]). %question
+%unifie([X ?= t, Y = X]). %question
 
 unifie([]).
-
-%\+unifie(bottom).
 
 unifie([E | P])
 :- unifie(P), reduit(_, E, P, Q), Q \= bottom.
 
 
 
-
+% select/4 éventuellement pour rename
 
 
 
